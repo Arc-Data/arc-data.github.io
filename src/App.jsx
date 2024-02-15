b0f94523f543fae5ee848352e5194594654f545b
Arc <arclight04@gmail.com>
2024-02-15 13:49:37 +0800

    base design?

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 function App() {
 	return (
-		<div className="text-3xl">
-			Arc
+		<div className="max-w-screen-xl min-h-screen mt-12 mx-auto px-6 py-12 justify-center text-white sm:text-center md:text-left">
+			<div>
+				<h1 className="text-2xl md:text-4xl lg:text-5xl font-bold text-cyan-400">Arc</h1>
+				<p className="text-lg text-slate-400 md:mt-3">Graduating from college soon...</p>
+				<p className="text-lg md:mt-3 text-slate-600">I build full stack Web Applications.</p>
+			</div>
 		</div>
 	)
 }
