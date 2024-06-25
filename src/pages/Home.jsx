5a68dd2faa9591c1ad4f9f02ad273570f8aa51c5
Arc <arclight04@gmail.com>
2024-06-25 20:28:15 +0800

    theme adjustments

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,20 +8,20 @@ const Home = () => {
     }
 
     return (
-        <div className="max-w-screen-xl min-h-screen px-6 py-12 mx-auto text-white md:px-12 md:py-20 sm:text-center md:text-left">
+        <div className="max-w-screen-xl px-6 py-12 mx-auto text-white md:px-12 md:py-20 sm:text-center md:text-left">
             <div className="md:flex md:justify-between md:gap-20">
                 <header className="pt-20 text-center md:text-left md:sticky md:top-20 md:flex md:flex-col md:max-h-screen md:w-1/4 ">
                     <div>
-                        <h1 className="text-6xl font-bold text-blue-500 md:text-4xl lg:text-5xl">Arc</h1>
-                        <p className="overflow-hidden text-lg md:mt-3 text-slate-400">I Build Full Stack Web Applications.</p>
-                        <p className="h-10 text-lg text-slate-600 md:mt-3">Graduating from College Soon</p>
+                        <h1 className="text-6xl font-bold text-primary-default md:text-4xl lg:text-6xl">Arc</h1>
+                        <p className="mt-3 overflow-hidden text-lg text-primary-default">I Build Full Stack Web Applications.</p>
+                        <p className="h-10 mt-3 text-lg text-text-700">Graduating from College Soon</p>
                         <nav className="hidden mt-3 md:grid *:w-full  *:border-blue-500 *:rounded md:gap-2">
-                            <button onClick={() => scrollIntoView("section1")} className="flex items-center gap-2 px-2 py-3 group hover:bg-blue-500">
-                                <span className="w-4 h-2 bg-blue-800 rounded group-hover:w-8 group-hover:bg-white"></span> 
+                            <button onClick={() => scrollIntoView("section1")} className="flex items-center gap-2 px-2 py-3 group hover:bg-accent-default">
+                                <span className="w-4 h-2 rounded bg-secondary-default group-hover:w-8 group-hover:bg-white"></span> 
                                 <p className="uppercase">About Me</p>
                             </button>
-                            <button onClick={() => scrollIntoView("section2")} className="flex items-center gap-2 px-2 py-3 group hover:bg-blue-500">
-                                <span className="w-4 h-2 transition ease-in-out bg-blue-800 rounded delay-10 group-hover:w-8 group-hover:bg-white"></span> 
+                            <button onClick={() => scrollIntoView("section2")} className="flex items-center gap-2 px-2 py-3 group hover:bg-accent-default">
+                                <span className="w-4 h-2 transition ease-in-out rounded bg-secondary-default delay-10 group-hover:w-8 group-hover:bg-white"></span> 
                                 <p className="uppercase">Projects</p>
                             </button>
                         </nav>
@@ -39,7 +39,7 @@ const Home = () => {
                 </header>
                 <main className="flex flex-col *:px-[1.5rem] gap-40 mt-10 text-sm md:w-3/4 md:flex-1 md:text-left">
                     <div className="grid gap-8 py-10 text-slate-200" id="section1">
-                        <h2 className="text-xl font-bold text-center uppercase md:text-left text-slate-500">About Me</h2>
+                        <h2 className="text-xl font-bold text-center uppercase md:text-left text-text-800">About Me</h2>
                         <p className="px-[1.5rem] indent-8 md:max-w-full mx-auto text-justify mt-3 text-sm leading-8 md:px-0 md:leading-8 md:text-base ">
                             I am a 4th-year college student expecting to graduate in a semester after OJT/Internships. I learned about HTML and CSS back when I was in grade 9 and that made me interested in programming in general. I had grown up to love knowing all the nerdy stuff, like a kid who discovers a manual for a potential lego masterpiece. 
                             I am looking forward to contribute to the community.
@@ -48,12 +48,12 @@ const Home = () => {
                             Outside of programming, my interests lie in anime, light novels and visual novels, and I have been studying Japanese language in my free time.
                         </p>
                         <div className="flex justify-center gap-4 md:justify-start">
-                            <button className="px-4 py-3 mt-6 uppercase bg-transparent border border-white rounded shadow-xl hover:border-cyan-500">Contact Me</button>
-                            <button className="px-4 py-3 mt-6 uppercase bg-blue-800 rounded shadow-xl hover:bg-blue-700">Download CV</button>
+                            <button className="px-8 py-2.5 mt-6 rounded shadow-xl bg-secondary-default hover:border-cyan-500">Contact Me</button>
+                            <button className="px-8 py-2.5 mt-6 rounded text-background-default bg-primary-default hover:shadow-primary-default hover:shadow-2xl">Download CV</button>
                         </div>
                     </div>
                     <div className="py-10 text-base text-slate-200" id="section2">
-                        <h2 className="text-xl font-bold text-center uppercase md:text-left text-slate-500">Featured Projects</h2>
+                        <h2 className="text-xl font-bold text-center uppercase md:text-left text-text-800">Featured Projects</h2>
                         <div className="grid mt-4 *:py-4">
                             <div>
                                 <div className="grid px-4 py-6 rounded group md:grid-cols-3 gap-x-5 gap-y-8 md:gap-y-8 hover:bg-slate-800 hover:cursor-pointer">
@@ -63,11 +63,11 @@ const Home = () => {
                                         <p className="mt-2 text-md">A travel planning website where interested travelers may create their own itinerary and also involves analyzing and matching user preferences to possible itineraries or recommended locations in Cebu. Capstone Research Project.</p>
                                         <div className="mt-4 flex justify-center md:justify-between gap-4 *:items-center *:flex *:gap-2">
                                             <div>
-                                                <i class="fa-solid fa-link"></i>    
+                                                <i className="fa-solid fa-link"></i>    
                                                 <p>Frontend Repository</p>
                                             </div>
                                             <div>
-                                                <i class="fa-solid fa-link"></i>    
+                                                <i className="fa-solid fa-link"></i>    
                                                 <p>Backend Repository</p>
                                             </div>
                                         </div>
