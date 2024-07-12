c039d4ca07c3ad1a1652850ae9b363c1b4ee7181
Arc <arclight04@gmail.com>
2024-07-12 18:40:56 +0800

    add temporary loading state

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,7 +12,8 @@ import { RiFirebaseFill } from "react-icons/ri"
 import { BiCaretRight } from "react-icons/bi"
 
 const Home = () => {
-    const [projects, setProjects] = useState([])
+    const [ projects, setProjects ] = useState([])
+    const [ loading, setLoading ] = useState(true)
     const { defaultScreenDetails } = useContext(PageContext)
 
     // const scrollIntoView = (sectionId) => {
@@ -29,6 +30,9 @@ const Home = () => {
             catch(error) {
                 console.log(error)
             }
+            finally {
+                setLoading(false)
+            }
         }
         defaultScreenDetails()
         fetchArticles()
@@ -43,61 +47,108 @@ const Home = () => {
                     I like building web technologies with the intention of being able to see myself as a fan or a consumer of it. <span className="text-xl text-accent-default">No cutting corners</span>, and aim to <span className="text-xl text-accent-default">deliver the best website experience possible</span>.  
                     {/* see web development as a field that lets me leverage and apply my passion for learning almost immediately. I am incredibly confident in my skills at the following:     */}
                 </p>
-                    <div className="grid justify-between grid-cols-4 gap-2">
-                        <div className="flex flex-col gap-2 p-2 text-xs border rounded place-items-center">
-                            <DiHtml5 size={24}/>
-                            <p>HTML</p>
-                        </div>
-                        <div className="flex flex-col gap-2 p-2 text-sm border rounded place-items-center">
-                            <SiCss3 size={24}/>
-                            <p>CSS</p>
-                        </div>
-                        <div className="flex flex-col gap-2 p-2 text-sm border rounded place-items-center">
-                            <SiJavascript size={24}/>
-                            <p>JavaScript</p>
-                        </div>
-                        <div className="flex flex-col gap-2 p-2 text-sm border rounded place-items-center">
-                            <FaReact size={24}/>
-                            <p>React</p>
-                        </div>
-                        <div className="flex flex-col gap-2 p-2 text-sm border rounded place-items-center">
-                            <SiTailwindcss size={24}/>
-                            <p>Tailwind</p>
-                        </div>
-                        <div className="flex flex-col gap-2 p-2 text-sm border rounded place-items-center">
-                            <DiDjango size={24}/>
-                            <p>Django</p>
-                        </div>
-                        <div className="flex flex-col gap-2 p-2 text-sm border rounded place-items-center">
-                            <DiLaravel size={24}/>
-                            <p>Laravel</p>
-                        </div>
-                        <div className="flex flex-col gap-2 p-2 text-sm border rounded place-items-center">
-                            <SiGithub size={24}/>
-                            <p>Github</p>
-                        </div>
+                <div className="grid justify-between grid-cols-4 gap-2">
+                    <div className="flex flex-col gap-2 p-2 text-xs border rounded place-items-center">
+                        <DiHtml5 size={24}/>
+                        <p>HTML</p>
                     </div>
-                    <div className="flex items-center justify-end mt-2 text-accent-default">
-                        <span>Show All </span> 
-                        <BiCaretRight />
+                    <div className="flex flex-col gap-2 p-2 text-sm border rounded place-items-center">
+                        <SiCss3 size={24}/>
+                        <p>CSS</p>
                     </div>
+                    <div className="flex flex-col gap-2 p-2 text-sm border rounded place-items-center">
+                        <SiJavascript size={24}/>
+                        <p>JavaScript</p>
+                    </div>
+                    <div className="flex flex-col gap-2 p-2 text-sm border rounded place-items-center">
+                        <FaReact size={24}/>
+                        <p>React</p>
+                    </div>
+                    <div className="flex flex-col gap-2 p-2 text-sm border rounded place-items-center">
+                        <SiTailwindcss size={24}/>
+                        <p>Tailwind</p>
+                    </div>
+                    <div className="flex flex-col gap-2 p-2 text-sm border rounded place-items-center">
+                        <DiDjango size={24}/>
+                        <p>Django</p>
+                    </div>
+                    <div className="flex flex-col gap-2 p-2 text-sm border rounded place-items-center">
+                        <DiLaravel size={24}/>
+                        <p>Laravel</p>
+                    </div>
+                    <div className="flex flex-col gap-2 p-2 text-sm border rounded place-items-center">
+                        <SiGithub size={24}/>
+                        <p>Github</p>
+                    </div>
+                </div>
+                <div className="flex items-center justify-end mt-2 text-accent-default">
+                    <span>Show All </span> 
+                    <BiCaretRight />
+                </div>
                 <p className="mx-auto mt-3 text-sm leading-8 text-justify md:px-0 md:max-w-full md:leading-8 md:text-base indent-8">
                     Outside of programming, I'm quite the reader and a Japanese media enthusiast. Plays a bit of DOTA and learns a bit of Nihongo on the side!
                 </p>
-                {/* <div className="flex justify-center gap-4 md:justify-start">
-                    <button className="px-8 py-2.5 mt-6 rounded shadow-xl bg-secondary-default hover:border-cyan-500">Contact Me</button>
-                    <button className="px-8 py-2.5 mt-6 rounded text-background-default bg-primary-default hover:shadow-primary-default hover:shadow-2xl">Download CV</button>
-                </div> */}
             </div>
             
             <div className="py-10 text-base text-text-default" id="section2">
                 <h2 className="text-xl font-bold text-center uppercase md:text-left text-text-800">Featured Projects</h2>
                 <div className="grid mt-4 *:py-4">
-                    <div>
-                        {projects.map(project => <FeaturedProject key={project.id} project={project} />)}
+                    {loading ? 
+                    <div className="grid gap-24 md:gap-8 place-items-center md:place-items-start">
+                        <div class="space-y-8 animate-pulse md:space-y-0 md:space-x-8 rtl:space-x-reverse md:flex md:items-center">
+                            <div class="flex items-center justify-center h-36 bg-gray-300 rounded sm:w-96 dark:bg-gray-700">
+                                <svg class="w-10 h-10 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+                                    <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z"/>
+                                </svg>
+                            </div>
+                            <div class="w-full">
+                                <div class="h-2.5 bg-gray-200 rounded-full w-48 mb-4"></div>
+                                <div class="h-2 bg-gray-200 rounded-full mb-2.5 "></div>
+                                <div class="h-2 bg-gray-200 rounded-full mb-2.5"></div>
+                                <div class="h-2 bg-gray-200 rounded-full mb-2.5"></div>
+                                <div class="h-2 bg-gray-200 rounded-full mb-2.5"></div>
+                                <div class="h-2 bg-gray-200 rounded-full "></div>
+                            </div>
+                            <span class="sr-only">Loading...</span>
+                        </div>
+                        <div class="space-y-8 animate-pulse md:space-y-0 md:space-x-8 rtl:space-x-reverse md:flex md:items-center">
+                            <div class="flex items-center justify-center h-36 bg-gray-300 rounded sm:w-96 dark:bg-gray-700">
+                                <svg class="w-10 h-10 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+                                    <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z"/>
+                                </svg>
+                            </div>
+                            <div class="w-full">
+                                <div class="h-2.5 bg-gray-200 rounded-full w-48 mb-4"></div>
+                                <div class="h-2 bg-gray-200 rounded-full mb-2.5 "></div>
+                                <div class="h-2 bg-gray-200 rounded-full mb-2.5"></div>
+                                <div class="h-2 bg-gray-200 rounded-full mb-2.5"></div>
+                                <div class="h-2 bg-gray-200 rounded-full mb-2.5"></div>
+                                <div class="h-2 bg-gray-200 rounded-full "></div>
+                            </div>
+                            <span class="sr-only">Loading...</span>
+                        </div>
+                        <div class="space-y-8 animate-pulse md:space-y-0 md:space-x-8 rtl:space-x-reverse md:flex md:items-center">
+                            <div class="flex items-center justify-center h-36 bg-gray-300 rounded sm:w-96 dark:bg-gray-700">
+                                <svg class="w-10 h-10 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+                                    <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z"/>
+                                </svg>
+                            </div>
+                            <div class="w-full">
+                                <div class="h-2.5 bg-gray-200 rounded-full w-48 mb-4"></div>
+                                <div class="h-2 bg-gray-200 rounded-full mb-2.5 "></div>
+                                <div class="h-2 bg-gray-200 rounded-full mb-2.5"></div>
+                                <div class="h-2 bg-gray-200 rounded-full mb-2.5"></div>
+                                <div class="h-2 bg-gray-200 rounded-full mb-2.5"></div>
+                                <div class="h-2 bg-gray-200 rounded-full "></div>
+                            </div>
+                            <span class="sr-only">Loading...</span>
+                        </div>
+                        
                     </div>
+                    :
+                    projects.map(project => <FeaturedProject key={project.id} project={project} />)
+                    }
                 </div>
-                <div className="text-right">View All</div>
             </div>
             <div className="py-10">
                 <h2 className="text-xl font-bold text-center uppercase md:text-left text-text-800">Developer History</h2>
